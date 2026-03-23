fcbdc5c57fabc411b4a022131d3db573eac4775a
amores <amores.dev@outlook.com>
2026-03-22 20:45:29 -0600

    feat: Implement the HelpdeskWorkflow class to define the agent's state graph using langgraph.

---
+++ src/graph/workflow.py
@@ -0,0 +1,50 @@
+from langgraph.graph import StateGraph, START, END
+from graph.state import HelpdeskState
+from graph.nodes import RagNode, ClassifyNode, EscalingNode, ResponseNode
+from graph.edges import decide_from_clasification, decide_since_human
+
+
+class HelpdeskWorkflow:
+    def __init__(self):
+        self.graph = None
+        self.rag_node = RagNode()
+        self.classify_node = ClassifyNode()
+        self.escaling_node = EscalingNode()
+        self.response_node = ResponseNode()
+
+    def build_graph(self):
+        self.graph = StateGraph(HelpdeskState)
+
+        self.graph.add_node("rag", self.rag_node.process_rag)
+        self.graph.add_node("classify", self.classify_node.classify_with_context)
+        self.graph.add_node("escalated", self.escaling_node.prepare_escaling)
+        self.graph.add_node(
+            "response_final", self.response_node.generate_final_response
+        )
+        self.graph.add_node("process_human", self.escaling_node.process_human_response)
+
+        self.graph.add_edge(START, "rag")
+        self.graph.add_edge("rag", "classify")
+
+        self.graph.add_conditional_edges(
+            "classify",
+            decide_from_clasification,
+            {
+                "response_final": "response_final",
+                "escalated": "escalated",
+            },
+        )
+
+        self.graph.add_conditional_edges(
+            "escalated",
+            decide_since_human,
+            {
+                "process_human_response": "process_human",
+                "waiting_human": END,  # Pause the graph until the human response
+            },
+        )
+
+        self.graph.add_edge("process_human", END)
+        self.graph.add_edge("response_final", END)
+
+        return self.graph.compile()
